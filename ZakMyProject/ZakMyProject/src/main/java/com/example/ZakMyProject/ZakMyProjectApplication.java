a6c6fe669de28427d8dda9846d49f35852c0d8fc
Zak <93520933+Zak12334@users.noreply.github.com>
2024-07-07 15:43:18 +0100

    Add files via upload

---
+++ ZakMyProject/ZakMyProject/src/main/java/com/example/ZakMyProject/ZakMyProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.ZakMyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakMyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZakMyProjectApplication.class, args);
+    }
+}
---
+++ ZakMyProject/ZakMyProject/src/test/java/com/example/ZakMyProject/ZakMyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZakMyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZakMyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
